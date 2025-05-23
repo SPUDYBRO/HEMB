52dd31468002fba61ea8c630d62bd986ef408173
SPUDYBRO <evanh3939@gmail.com>
2025-05-23 19:07:02 +1000

    feat: login_handle completed
    I have finished all the checks and created a session for the user upon successful login

--- php/login_handle.php
+++ php/login_handle.php
@@ -1,11 +1,30 @@
-<?php include "functionality.php"?>
+<?php
+
+    class User {
+        public $id;
+        public $username;
+        public $role;
+
+        function __construct($id, $username, $role) {
+            $this->id = $id;
+            $this->username = htmlspecialchars($username);
+            $this->role = $role;
+        }
+
+    }
+
+
+
+   include "functionality.php";
+    require_once "session.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/styles.css">
-    <title>Loggin in</title>
+    <title>Login in</title>
 </head>
 <body id="post_request">
 <?php 
@@ -20,6 +39,15 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
 
 
 date_default_timezone_set('Australia/Melbourne');
+
+
+ 
+if (isset($_SESSION['User'])) { # the user is already logged in...
+    session_regenerate_id(true);
+    header("Location: ../project1/manage.php");
+    die();
+}
+
 
 $error = [];
 
@@ -78,6 +106,51 @@ if (empty($_POST['password'])) {
 
 
 
+
+
+
+// Check if the user has submitted the correct username and password
+
+    $db = mysqli_connect($host, $user, $pwd, $sql_db);
+
+    if (!$db) { # the connection failed
+        set_data_response("error", 
+        "Error", 
+        "Database connection error", 
+        "The database connection failed", 
+        "There was an error connecting to the database", 
+        "The following is the error message:<br><pre>" . mysqli_connect_error() . "</pre>");
+        header("Location: ../project1/login.php");
+        die();
+    }
+
+
+    $stmt = $db->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->bind_param("s", $_POST['username']);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows == 0) { # That username doesn't exist!
+        $error[] = "The username is incorrect";
+    }
+
+    if ($result->num_rows > 1) { # That username exists more then once!
+        $error[] = "The username exists more then once"; # This should never happen though
+    }
+
+
+    $row = $result->fetch_assoc();
+
+
+    if (!password_verify($_POST['password'], $row['password'])) { # The password is incorrect
+        $error[] = "The password is incorrect";
+    }
+
+
+
+    $db->close(); #if the code reaches this point without errors then let them in. but if there are errors then deal with it
+
+
 // There was an error somewhere, so set the error message and return back to the login page
 if (count($error) > 0) {
 
@@ -102,5 +175,11 @@ if (count($error) > 0) {
     "The following are the errors that were found:<br><pre>" . $error_msg . "</pre>", 
     ["Sensitive data, no debug available" => "No data"]);
     header("Location: ../project1/login.php");
+    die();
+} else {
+    $_SESSION['User'] = new User($row['ID'], $row['Username'], $row['Role']);
+    session_regenerate_id(true);
+    $_SESSION['login_requests'] = 0;
+    header("Location: ../project1/manage.php");
     die();
 }
