adbf2927e8c0e26987adad9644dba5b50c3b2a73
SPUDYBRO <evanh3939@gmail.com>
2025-05-25 11:48:54 +1000

    feat: Created account management
    I have created a system that allows only administrator accounts to update the information or delete an account entirely. These admins have the right to create other admin accounts and delete other admin accounts. Ontop of this they are able to Change the password of a account (not view it though)

--- php/functionality.php
+++ php/functionality.php
@@ -10,6 +10,25 @@
         'Email' => '<svg xmlns="http://www.w3.org/2000/svg" width="800px" height="800px" viewBox="0 0 24 24" fill="none" class="nav_icon"><path d="M4 7L10.94 11.3375C11.5885 11.7428 12.4115 11.7428 13.06 11.3375L20 7M5 18H19C20.1046 18 21 17.1046 21 16V8C21 6.89543 20.1046 6 19 6H5C3.89543 6 3 6.89543 3 8V16C3 17.1046 3.89543 18 5 18Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
         'Staff' => '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="currentColor"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M15 16C14.7164 14.8589 13.481 14 12 14C10.519 14 9.28364 14.8589 9 16M11 3H13M12 10H12.01M8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.0799 3 8.2V16.8C3 17.9201 3 18.4802 3.21799 18.908C3.40973 19.2843 3.71569 19.5903 4.09202 19.782C4.51984 20 5.0799 20 6.2 20H17.8C18.9201 20 19.4802 20 19.908 19.782C20.2843 19.5903 20.5903 19.2843 20.782 18.908C21 18.4802 21 17.9201 21 16.8V8.2C21 7.07989 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H16M13 10C13 10.5523 12.5523 11 12 11C11.4477 11 11 10.5523 11 10C11 9.44772 11.4477 9 12 9C12.5523 9 13 9.44772 13 10Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>'
     ]);
+
+
+
+
+    class User {
+        public $ID;
+        public $Username;
+        public $Role;
+
+        function __construct($id, $username, $role) {
+            $this->ID = $id;
+            $this->Username = htmlspecialchars($username);
+            $this->Role = $role;
+        }
+
+    }
+
+
+
     function set_accessibility(): void {
         if (!isset($_SESSION['accessibility']) || !isset($_SESSION['accessibility']['colour_scheme']) || !isset($_SESSION['accessibility']['text_size'])) {
             $_SESSION['accessibility']["colour_scheme"] = 'default';
@@ -137,3 +156,36 @@
         ];
     }
 
+
+
+    /**
+     * Checks if the user is logged in
+     * 
+     * @return bool `True` if the user is logged in, `false` if not
+     */
+    function is_logged_in() {
+        if (!isset($_SESSION['User'])) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Logs a user out, retaining the accessibility settings
+     * Can only be called if headers haven't been sent yet
+     * 
+     * @return void
+     */
+    function logout() {
+        if (isset($_SESSION['PHP_RESPONSE'])) {
+            $php_response = $_SESSION['PHP_RESPONSE'];
+        }
+        $accessibility = $_SESSION['accessibility'];
+        session_unset();
+        session_destroy();
+        session_start();
+        $_SESSION['accessibility'] = $accessibility;
+        if (isset($php_response)) {
+            $_SESSION['PHP_RESPONSE'] = $php_response;
+        }
+    }
---
+++ php/process_manage.php
@@ -0,0 +1,187 @@
+
+<?php
+    require_once "settings.php";
+    include "functionality.php";
+
+
+
+    if (!is_logged_in()) {
+        set_data_response('error', 'Access Denied', 'You must be logged in to access this page', 'Access Denied', 'You must be logged in to access this page', '', $_POST);
+        header('Location: ../project1/index.php');
+        die();
+    }
+
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+
+        // ------------------------------------ EOI -------------------------------------
+
+            if (isset($_POST['EOI_Update'])) {
+
+                $db = mysqli_connect($host, $user, $pwd, $sql_db);
+                if (!$db) {
+                    set_data_response('error', 'Database Error', 'failed to connect to the database', 'Failed to connect to the database', "Something went wrong and failed to connect to the database", "Error: <pre>" . mysqli_connect_error() . "</pre>", $_POST);
+                    header('Location: manage.php?Mode=EOI');
+                    die();
+                }
+
+                $stmt = $db->prepare("UPDATE eoi SET Status = ? WHERE EOInumber = ?");
+                $stmt->bind_param("si", $_POST['status'], $_POST['EOInumber']);
+
+                $stmt->execute();
+
+                if ($stmt->affected_rows > 0) {
+                    set_data_response('success', 'EOI Updated', 'The EOI has been updated successfully', 'EOI Updated', 'The EOI has been updated successfully', '', $_POST);
+                    header('Location: ../project1/manage.php?Mode=EOI');
+                } else {
+                    set_data_response('error', 'EOI Update Failed', 'Failed to update the EOI', 'EOI Update Failed', 'Failed to update the EOI, please try again later', '', $_POST);
+                    header('Location: ../project1/manage.php?Mode=EOI');
+                }
+            }
+            elseif (isset($_POST['EOI_Delete'])) {
+
+                $db = mysqli_connect($host, $user, $pwd, $sql_db);
+                if (!$db) {
+                    set_data_response('error', 'Database Error', 'failed to connect to the database', 'Failed to connect to the database', "Something went wrong and failed to connect to the database", "Error: <pre>" . mysqli_connect_error() . "</pre>", $_POST);
+                    header('Location: ../project1/manage.php?Mode=EOI');
+                    die();
+                }
+
+                $stmt = $db->prepare("DELETE FROM eoi WHERE EOInumber = ?");
+                $stmt->bind_param("i", $_POST['EOInumber']);
+
+                $stmt->execute();
+
+                if ($stmt->affected_rows > 0) {
+                    set_data_response('success', 'EOI Deleted', 'The EOI has been deleted successfully', 'EOI Deleted', 'The EOI has been deleted successfully', '', $_POST);
+                    header('Location: ../project1/manage.php?Mode=EOI');
+                } else {
+                    set_data_response('error', 'EOI Deletion Failed', 'Failed to delete the EOI', 'EOI Deletion Failed', 'Failed to delete the EOI, please try again later', '', $_POST);
+                    header('Location: ../project1/manage.php?Mode=EOI');
+                }
+            }
+
+        
+        // ------------------------------------ Account Management -------------------------------------
+
+            if (isset($_POST['Account_Update'])) {
+                if ($_SESSION['User']->Role != 'Admin') {
+                    set_data_response('error', 'Access Denied', 'You do not have permission to update accounts', 'Access Denied', 'You do not have permission to update accounts', '', $_POST);
+                    header('Location: ../project1/manage.php?Mode=Accounts');
+                    die();
+                }
+
+
+                $db = mysqli_connect($host, $user, $pwd, $sql_db);
+                if (!$db) {
+                    set_data_response('error', 'Database Error', 'failed to connect to the database', 'Failed to connect to the database', "Something went wrong and failed to connect to the database", "Error: <pre>" . mysqli_connect_error() . "</pre>", $_POST);
+                    header('Location: manage.php?Mode=Account');
+                    die();
+                }
+
+                if (isset($_POST['Password']) || empty($_POST['Password'])) {
+                    $query = "UPDATE users SET Username = ?, Password = ?, Role = ? WHERE ID = ?";
+                    $password = password_hash($_POST['Password'], PASSWORD_DEFAULT);
+                } else {
+                    $query = "UPDATE users SET Username = ?, Role = ? WHERE ID = ?";
+                }
+                $stmt = $db->prepare($query);
+                if (isset($_POST['Password']) || empty($_POST['Password'])) {
+                    $stmt->bind_param("sssi", $_POST['Username'], $password, $_POST['Role'], $_POST['ID']);
+                } else {
+                    $stmt->bind_param("ssi", $_POST['Username'], $_POST['Role'], $_POST['ID']);
+                }
+
+                $stmt->execute();
+
+                if ($stmt->affected_rows > 0) {
+                    set_data_response('success', 'Account Updated', 'The account has been updated successfully', 'Account Updated', 'The account has been updated successfully', '', $_POST);
+                    header('Location: ../project1/manage.php?Mode=Accounts');
+                } else {
+                    set_data_response('error', 'Account Update Failed', 'Failed to update the account', 'Account Update Failed', 'Failed to update the account, please try again later', '', $_POST);
+                    header('Location: ../project1/manage.php?Mode=Accounts');
+                }
+            }
+
+
+            elseif (isset($_POST['Account_Create'])) {
+                if ($_SESSION['User']->Role != 'Admin') {
+                    set_data_response('error', 'Access Denied', 'You do not have permission to create accounts', 'Access Denied', 'You do not have permission to create accounts', '', $_POST);
+                    header('Location: ../project1/manage.php?Mode=Accounts');
+                    die();
+                }
+
+                $db = mysqli_connect($host, $user, $pwd, $sql_db);
+                if (!$db) {
+                    set_data_response('error', 'Database Error', 'failed to connect to the database', 'Failed to connect to the database', "Something went wrong and failed to connect to the database", "Error: <pre>" . mysqli_connect_error() . "</pre>", $_POST);
+                    header('Location: manage.php?Mode=Account');
+                    die();
+                }
+
+                $stmt = $db->prepare("INSERT INTO users (Username, Password, Role) VALUES (?, ?, ?)");
+                $password = password_hash($_POST['Password'], PASSWORD_DEFAULT);
+                $stmt->bind_param("sss", $_POST['Username'], $password, $_POST['Role']);
+
+                $stmt->execute();
+
+                if ($stmt->affected_rows > 0) {
+                    set_data_response('success', 'Account Created', 'The account has been created successfully', 'Account Created', 'The account has been created successfully', '', $_POST);
+                    header('Location: ../project1/manage.php?Mode=Accounts');
+                } else {
+                    set_data_response('error', 'Account Creation Failed', 'Failed to create the account', 'Account Creation Failed', 'Failed to create the account, please try again later', '', $_POST);
+                    header('Location: ../project1/manage.php?Mode=Accounts');
+                }
+            }
+            elseif (isset($_POST['Account_Delete'])) {
+                if ($_SESSION['User']->Role != 'Admin') {
+                    set_data_response('error', 'Access Denied', 'You do not have permission to delete accounts', 'Access Denied', 'You do not have permission to delete accounts', '', $_POST);
+                    header('Location: ../project1/manage.php?Mode=Accounts');
+                    die();
+                }
+
+                if ($_SESSION['User']->ID == $_POST['ID']) {
+                    set_data_response('error', 'Account Deletion Failed', 'You cannot delete your own account', 'Account Deletion Failed', 'You cannot delete your own account, please contact an administrator', '', $_POST);
+                    header('Location: ../project1/manage.php?Mode=Accounts');
+                    die();
+                }
+
+                $db = mysqli_connect($host, $user, $pwd, $sql_db);
+                if (!$db) {
+                    set_data_response('error', 'Database Error', 'failed to connect to the database', 'Failed to connect to the database', "Something went wrong and failed to connect to the database", "Error: <pre>" . mysqli_connect_error() . "</pre>", $_POST);
+                    header('Location: manage.php?Mode=Account');
+                    die();
+                }
+
+                $stmt = $db->prepare("DELETE FROM users WHERE ID = ?");
+                $stmt->bind_param("i", $_POST['ID']);
+
+                $stmt->execute();
+
+                if ($stmt->affected_rows > 0) {
+                    set_data_response('success', 'Account Deleted', 'The account has been deleted successfully', 'Account Deleted', 'The account has been deleted successfully', '', $_POST);
+                    header('Location: ../project1/manage.php?Mode=Accounts');
+                } else {
+                    set_data_response('error', 'Account Deletion Failed', 'Failed to delete the account', 'Account Deletion Failed', 'Failed to delete the account, please try again later', '', $_POST);
+                    header('Location: ../project1/manage.php?Mode=Accounts');
+                }
+            }
+
+
+
+
+
+
+
+
+
+            if (isset($db)) {
+                $db->close();
+            }
+
+
+    }
+
+
+
+?>
